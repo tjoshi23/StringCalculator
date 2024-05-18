93ac353c8df99ebba80af0eef3ed372904f74792
tanmay joshi <tanmay.joshi@gethownow.com>
2024-05-18 09:20:17 +0530

    return the number itself for singlre number

--- app/models/string_calculate.rb
+++ app/models/string_calculate.rb
@@ -1,5 +1,9 @@
 class StringCalculate
   def add(numbers)
     return 0 if numbers.empty?
+
+    number_list = numbers.split("").map(&:to_i)
+
+    number_list.sum
   end
 end
--- spec/models/string_calculate_spec.rb
+++ spec/models/string_calculate_spec.rb
@@ -3,9 +3,13 @@ require 'string_calculate'
 
 
 RSpec.describe StringCalculate, type: :model do
+  
   it 'returns 0 for an empty string' do
     expect(StringCalculate.new.add("")).to eq(0)
   end
 
-  
+  it 'returns the number itself for a single number' do
+    expect(StringCalculate.new.add("1")).to eq(1)
+    expect(StringCalculate.new.add("5")).to eq(5)
+  end
 end
